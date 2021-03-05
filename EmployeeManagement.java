7292055facaa6a08b8a29bbcf2cd70bf3c703234
vignesh-2812196 <vignesh.ramesh@ideas2it.com>
2021-03-05 19:29:32 +0530

    mvc commit

---
+++ EmployeeManagement.java
@@ -0,0 +1,13 @@
+import com.ideas2it.employee.view.EmployeeView;
+
+/**
+ * CRUD operation using collection
+ * @author vignesh r
+ * @version1.0 03-03-2021
+ */
+public class EmployeeManagement {
+    public static void main(String[] args) {
+	EmployeeView employeeView = new EmployeeView();
+	employeeView.operation();
+    }
+}
